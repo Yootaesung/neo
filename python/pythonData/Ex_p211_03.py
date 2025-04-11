8730d2bbf0592e8338266f60780a54cc4a6c2331
Yootaesung <xotjdgg@gmail.com>
2025-04-11 15:29:08 +0900

    pandas

---
+++ python/pythonData/Ex_p211_03.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from pandas import DataFrame, Series
+
+filename = '과일매출현황.csv'
+df = pd.read_csv(filename, index_col='과일명', encoding='utf-8')
+print('\n원본 데이터프레임')
+print(df)
+print('-' * 50)
+
+print('\n 구입액과 수입량의 소개')
+print(df.sum(axis=0))
+print('-' * 50)
+
+print('\n 과일별 소개')
+print(df.sum(axis=1))
+print('-' * 50)
+
+print('\n 구입액과 수입량의 평균')
+print(np.round(df.mean(axis=0), 2))
+print('-' * 50)
+
+print('\n 과일별 평균')
+print(np.round(df.mean(axis=1), 2))
+print('-' * 50)
