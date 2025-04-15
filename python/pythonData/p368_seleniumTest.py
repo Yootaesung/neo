92a0ab8ba7bd9afc763265346e8ea806917f038d
Yootaesung <xotjdgg@gmail.com>
2025-04-15 11:11:08 +0900

    selenium

--- python/pythonData/p368_seleniumTest.py
+++ python/pythonData/p368_seleniumTest.py
@@ -4,10 +4,10 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 print(type(driver))
-print('-' * 50)
+print('-' * 50) 
 
-print('Go Google~!!')
-url = www.google.com
+print('Go Goole~!!')
+url = 'http://www.google.com'
 driver.get(url)
 
 search_textbook = driver.find_element(By.NAME, 'q')
@@ -17,3 +17,15 @@ search_textbook.send_keys(word)
 search_textbook.submit()
 
 wait = 3
+print(str(wait) + ' seconds later...')
+time.sleep(wait)
+
+imagefile = 'p368_selenium.png'
+driver.save_screenshot(imagefile)
+print(imagefile + ' saved')
+
+wait = 3
+driver.implicitly_wait(wait)
+
+driver.quit()
+print('Brwowser closed')
